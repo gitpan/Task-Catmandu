f0c1616fcd99812dd4221d0dac306e0ef1b4bcd8
Nicolas Steenlant <nicolas.steenlant@ugent.be>
2013-05-21 08:09:41 +0000

    Import of NICS/Task-Catmandu-0.02 from CPAN.
    gitpan-cpan-distribution: Task-Catmandu
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         NICS/Task-Catmandu-0.02.tar.gz
    gitpan-cpan-author:       NICS
    gitpan-cpan-maturity:     released

--- lib/Task/Catmandu.pm
+++ lib/Task/Catmandu.pm
@@ -1,96 +1,12 @@
 package Task::Catmandu;
 
 use strict;
-use 5.008_001;
-our $VERSION = '0.0102';
-use ExtUtils::MakeMaker;
+use warnings;
 
-sub dependencies {
-    return (
-        'repl (interactive environment)', 1, [
-            [ 'Catmandu::Cmd::repl' ],
-        ],
-        'MongoDB store', 1, [
-            [ 'Catmandu::Store::MongoDB' ],
-        ],
-        'ElasticSearch store', 1, [
-            [ 'Catmandu::Store::ElasticSearch' ],
-        ],
-        'Solr store', 1, [
-            [ 'Catmandu::Store::Solr' ],
-        ],
-        'FedoraCommons store', 1, [
-            [ 'Catmandu::FedoraCommons' ],
-        ],
-        'MARC importer and MARC fixes', 1, [
-            [ 'Catmandu::MARC' ],
-        ],
-        'bibliographic importers', 1, [
-            [ 'Catmandu::PubMed' ],
-            [ 'Catmandu::ArXiv' ],
-            [ 'Catmandu::PLoS' ],
-        ],
-        'OAI importer', 1, [
-            [ 'Catmandu::OAI' ],
-        ],
-        'SRU importer', 1, [
-            [ 'Catmandu::SRU' ],
-        ],
-        'Z39.50 importer', 0, [
-            [ 'Catmandu::Z3950' ],
-        ],
-        'Twitter importer', 0, [
-            [ 'Catmandu::Importer::Twitter' ],
-        ],
-        'LDAP importer', 0, [
-            [ 'Catmandu::LDAP' ],
-        ],
-        'Dancer integration', 0, [
-            [ 'Dancer::Session::Catmandu' ],
-            [ 'Dancer::Plugin::Catmandu::OAI' ],
-            [ 'Dancer::Plugin::Catmandu::SRU' ],
-        ],
-    );
-}
-
-sub has_module {
-    my $file = shift;
-    $file =~ s!::!/!g;
-    scalar grep -e "$_/$file.pm", @INC;
-}
-
-sub iter_deps {
-    my($class, $cb) = @_;
-    my @deps = $class->dependencies;
-    while (my($name, $cond, $deps) = splice @deps, 0, 3) {
-        $cb->($name, $cond, $deps);
-    }
-}
-
-sub makefile_pl {
-    my $class = shift;
-    my $fh = shift;
-
-    $class->iter_deps(sub {
-        my($name, $cond, $deps) = @_;
-        my @modules = grep defined, map $_->[0], @$deps;
-        $fh->print("feature '$name', -default => $cond,\n");
-        for my $module (@modules) {
-            $fh->print("  '$module', '", version_for($module), "',\n");
-        }
-        $fh->print(";\n");
-    });
-}
-
-sub version_for {
-    my $dist = shift;
-
-    (my $module = $dist) =~ s/-/::/g;
-    my $info = `cpanm --info $module ` or return;
-    return ($info =~ /-([\d\.]+)\.tar\.gz/)[0];
-}
+our $VERSION = '0.02';
 
 1;
+
 __END__
 
 =encoding utf-8
@@ -117,9 +33,5 @@ it under the same terms as Perl itself.
 =head1 SEE ALSO
 
 L<http://librecat.org/>
-
-=head1 ACKNOWLEDGEMENTS
-
-Module code is taken from L<Task::Plack>.
 
 =cut
